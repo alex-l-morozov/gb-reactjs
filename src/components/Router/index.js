c8bd756c9e296029f797d777e12b10c27f358513
Alexander Morozov <mail@alexandermorozov.ru>
2021-08-04 09:19:30 +0300

    lesson 4

---
+++ src/components/Router/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import Home from '../Home';
+import Profile from '../Profile';
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <ul>
+                <li>
+                    <Link to='/home'>HOME</Link>
+                </li>
+                <li>
+                    <Link to='/profile'>Profile</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path='/' exact>
+                    <h1>Welcome</h1>
+                </Route>
+                <Route path='/home/:chatCode?'>
+                    <Home />
+                </Route>
+                <Route path='/profile'>
+                    <Profile />
+                </Route>
+                <Route path="*">
+                    <h1>404</h1>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
