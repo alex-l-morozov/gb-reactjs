acaa9f6345457f0399f253dac61f20518338cf5b
Alexander Morozov <mail@alexandermorozov.ru>
2021-08-05 20:00:06 +0300

    lesson 5

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import {store} from "../../store";
+//import {store} from "../../store";
 import {PROFILE_TOGGLE_SHOW} from "../../store/actionTypes";
+import {useSelector, useDispatch} from "react-redux";
 
 function Profile() {
-    const profileState = store.getState();
+    //const profileState = store.getState();
+    const profileState = useSelector(state => state);
+    const dispatch = useDispatch();
 
-    console.log(profileState);
+    //console.log(profileState);
 
     const toggleShow = () => {
-        store.dispatch({
+        // store.dispatch({
+        //     type: PROFILE_TOGGLE_SHOW
+        // });
+        dispatch({
             type: PROFILE_TOGGLE_SHOW
         });
     }
 
     return (
-        <>
+        <div>
             <h2>
                 PROFILE
             </h2>
-            <input type="checkbox" onClick={toggleShow} /> Тест
-        </>
-
+            <input type="checkbox" onClick={toggleShow} /> Показать / спрятать Имя пользователя
+            {profileState.show && <div>Имя: Александр</div>}
+        </div>
     );
 }
 
